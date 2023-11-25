b0a86830d5f901d1d20a5b1525f3ff1ac95b7980
Abhijeet <132761740+Abhijeetkrs@users.noreply.github.com>
2023-11-25 22:21:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
-const express = require("express");
-const colors = require("colors");
-const moragan = require("morgan");
-const dotenv = require("dotenv");
-const userRoutes = require('./routes/userRoute');
-const adminRoutes = require("./routes/adminRoute");
-const doctorRoutes = require("./routes/doctorRoute")
-const connectDb = require("./config/connectDb");
-const path = require("path");
+import "antd/dist/reset.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import store from "./redux/store";
 
-//dotenv conig
-dotenv.config();
-connectDb();
 
-//rest obejct
-const app = express();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
 
-//middlewares
-app.use(express.json());
-app.use(moragan("dev"));
-
-//routes
-app.use("/api/user", userRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/doctor", doctorRoutes);
-
-//static files
-app.use(express.static(path.join(__dirname, "./client/build")));
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-//port
-const PORT = process.env.PORT || 4001;
-//listen port
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
